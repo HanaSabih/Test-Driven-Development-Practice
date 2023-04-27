df8607929a100116ebb8b8830c46de4b7dacca1d
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-04-27 14:42:29 +0700

    Fix lint err

--- solver.rb
+++ solver.rb
@@ -1,18 +1,19 @@
 class Solver
-    def factorial(num)
-      raise 'Number cannot be Negative' if num.negative?
-  
-      result = 1
-      while num.positive?
-        result *= num
-        num -= 1
-      end
-      result
+  def factorial(num)
+    raise 'Number cannot be Negative' if num.negative?
+
+    result = 1
+    while num.positive?
+      result *= num
+      num -= 1
     end
-  
-    def reverse(str)
-      raise ArgumentError, 'string must be a string' unless str.is_a? String
-      str.reverse
-    end
-    
+    result
+  end
+
+  def reverse(str)
+    raise ArgumentError, 'string must be a string' unless str.is_a? String
+
+    str.reverse
+  end
+
 end
